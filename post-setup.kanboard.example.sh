63e3493cacc57a8ae7f1d4cce9b4352618cf0996
Sergejs Zablockis <zablose@gmail.com>
2024-09-14 13:58:12 +0100

    Append 'env.conf' with Kanboard envs to 'www.conf' file.

--- post-setup.kanboard.example.sh
+++ post-setup.kanboard.example.sh
@@ -10,7 +10,11 @@ bin=/usr/local/bin
 . "${bin}/functions.sh"
 
 web_root_dir=${ZDI_WEB_ROOT_DIR}
+version=${ARG_VERSION_PHP}
+
+dir_php=/etc/php/${version}
 file=https://github.com/kanboard/kanboard/archive/refs/tags/v1.2.39.tar.gz
+env_conf=https://github.com/kanboard/kanboard/blob/main/docker/etc/php83/php-fpm.d/env.conf
 log=/var/log/zdi-post-setup-php-fpm.log
 
 {
@@ -20,6 +24,8 @@ log=/var/log/zdi-post-setup-php-fpm.log
     wget ${file} --output-document=kanboard.tgz && \
     tar xzf kanboard.tgz -C "${web_root_dir}" --strip-components=1 && \
     rm kanboard.tgz
+
+    sudo bash -c "wget ${env_conf} -O ->> ${dir_php}/fpm/pool.d/www.conf"
 
     r-web "${ZDI_WEB_ROOT_DIR}"
 
