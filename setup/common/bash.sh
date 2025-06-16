5aba8e286fb1552c8703e380b3dfd294242e0a8c
Sergejs Zablockis <zablose@gmail.com>
2025-06-16 16:20:11 +0100

    Remove bash prompts from the bash.sh setup file, the bash-prompt.sh script does it already.

--- setup/common/bash.sh
+++ setup/common/bash.sh
@@ -11,24 +11,6 @@ log=/var/log/zdi-bash.log
 {
     show_info 'Setting up Bash.'
 
-    tee -a /root/.bashrc <<EOF
-
-# Set a fancy prompt
-if [ "\`id -u\`" -eq 0 ]; then
-    PS1='\[\033[0;90m\][\[\033[2;91m\]\u\[\033[0;90m\]@\[\033[2;92m\]\h\[\033[0;90m\]][\[\033[0;94m\]\W\[\033[0;90m\]]\[\033[2;91m\]\\$\[\033[00m\] '
-fi
-
-EOF
-
-    tee -a /etc/skel/.bashrc <<EOF
-
-# set a fancy prompt (overwrite the one in /etc/profile)
-if [ "\`id -u\`" -ne 0 ]; then
-    PS1='\[\033[0;90m\][\[\033[2;93m\]\u\[\033[0;90m\]@\[\033[2;92m\]\h\[\033[0;90m\]][\[\033[0;94m\]\W\[\033[0;90m\]]\[\033[2;93m\]\\$\[\033[00m\] '
-fi
-
-EOF
-
     tee -a /etc/bash.bashrc <<EOF
 
 # Aliases for all users.
